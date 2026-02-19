8831b1b9a1ac77fd7dddf2ca57f18bc325702419
mohammad <hgitinejad@gmail.com>
2026-02-19 15:00:22 +0100

    fixed

--- main.tf
+++ main.tf
@@ -81,9 +81,9 @@ module "eks" {
       instance_types = ["t3a.medium"]
       capacity_type  = "ON_DEMAND"
 
-      min_size     = 1
-      max_size     = 1
-      desired_size = 1
+      min_size     = 2
+      max_size     = 3
+      desired_size = 2
 
       block_device_mappings = {
         xvda = {
